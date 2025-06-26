02cd867e5383c377fac0e826e2134e99278807c0
jaabless <jefferyasamoahbless@gmail.com>
2025-06-26 14:18:51 +0000

    Create signupPage with Page Factory pattern

---
+++ src/main/java/org/example/pages/SignupPage.java
@@ -0,0 +1,40 @@
+package org.example.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SignupPage {
+    private WebDriver driver;
+
+    @FindBy(id = "email")
+    private WebElement emailInput;
+
+    @FindBy(className = "subscribe-btn")
+//    private By subscribeButton = new By.ByClassName("subscribe-btn");
+    private WebElement subscribeButton;
+
+    @FindBy(className = "error-message")
+    private WebElement errorMessage;
+
+    public SignupPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void enterEmail(String email) {
+        emailInput.clear();
+        emailInput.sendKeys(email);
+    }
+
+    public void clickSubscribe() {
+        subscribeButton.click();
+    }
+
+
+    public String getErrorMessage() {
+        return errorMessage.getText();
+    }
+}
